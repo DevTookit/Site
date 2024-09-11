e4d21ced98af9be628e073bd708e3c29a5d612e9
wooinwoo <wooin1001@gmail.com>
2024-09-12 02:28:42 +0900

    #1 퍼블리싱 테스트 화면 제작

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -4,13 +4,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // 페이지 컴포넌트 임포트
 import LoginPage from '@/pages/Login';
+import ProfilePage from '@/pages/Profile';
+import PublishPage from '@/pages/Test/Publish';
 
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<PublishPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/publish" element={<PublishPage />} />
         {/* 404 페이지 처리 */}
         <Route path="*" element={<LoginPage />} />
       </Routes>
