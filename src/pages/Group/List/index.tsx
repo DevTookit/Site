ac6fdd4144cf0da6be363c9a10c3d2fb6e761654
wooinwoo <wooin1001@gmail.com>
2024-09-28 23:20:42 +0900

    #13 리스트화면 퍼블리싱(미완)

--- src/pages/Group/List/index.tsx
+++ src/pages/Group/List/index.tsx
@@ -9,6 +9,11 @@ import { useState } from 'react';
 
 const GroupList: React.FC = () => {
   const [fileModalisOpen, setFileModalisOpen] = useState(false);
+  const [list, setList] = useState<number[]>([]);
+
+  const notEmpty = () => {
+    setList([1, 2, 3]);
+  };
   return (
     <>
       <CreateFileModal
@@ -18,13 +23,47 @@ const GroupList: React.FC = () => {
       <div className="flex w-full flex-1 flex-col">
         <div className="mb-1 mt-6 flex justify-start">
           <Breadcrumb items={['CSS', 'Animation', 'Modal']} />
+          <button
+            className="ml-10 flex h-10 items-center rounded-lg bg-black p-4 text-white"
+            onClick={notEmpty}
+          >
+            notEmpty(누르면 리스트 노출)
+          </button>
         </div>
         <h4 className="text-[32px] font-bold text-lighten-500">{'Modal'}</h4>
         <div className="mb-[6px] mt-6 flex justify-between">
           <UploadButton onClickFn={() => setFileModalisOpen(true)} />
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <ShowEmptyFile />
+          {list.length === 0 ? (
+            <ShowEmptyFile />
+          ) : (
+            <div className="mt-3 flex h-full w-full flex-col items-start">
+              <ul className="flex">
+                <li className="flex h-[42px] w-[124px] items-center justify-center rounded-t-xl bg-[#D9D9D9] text-lg font-bold text-lighten-400">
+                  전체
+                </li>
+                <li className="flex h-[42px] w-[124px] items-center justify-center rounded-tr-xl bg-darken-100 text-lg font-bold text-lighten-400">
+                  폴더
+                </li>
+                <li className="flex h-[42px] w-[124px] items-center justify-center rounded-tr-xl bg-darken-200 text-lg font-bold text-lighten-400">
+                  코드
+                </li>
+                <li className="flex h-[42px] w-[124px] items-center justify-center rounded-tr-xl bg-darken-300 text-lg font-bold text-lighten-400">
+                  게시판
+                </li>
+              </ul>
+              <ul>
+                <li></li>
+                <li>유형</li>
+                <li>제목</li>
+                <li>작성자</li>
+                <li>업로드</li>
+                <li>용량</li>
+                <li>북마크</li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </>
